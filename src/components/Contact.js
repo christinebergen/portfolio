572efaf8fed5aae3afcf005b7b975c6ab8a8cd32
christinebergen <christine.bergen@itas.ca>
2024-08-20 21:23:20 -0700

    update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,7 @@ import SwirlyLine from "./SwirlyLine";
 const Contact = () => {
   return (
     <div className="py-8 w-full md:w-3/4 flex justify-center items-center">
-      <div className="flex flex-col justify-center items-center">
+      <div className="min-h-screen flex flex-col justify-center items-center">
         <div className=" bg-zinc-200 rounded-lg flex flex-col justify-center items-center p-40">
           <h1 className="text-3xl text-primary">Contact Me</h1>
           <SwirlyLine/>
